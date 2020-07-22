47a17e3b5e4210972d24ceba6d677d0601312796
Hyungjun, Jeon <Hyungjun, Jeon@RedRooster>
2020-07-22 22:27:16 +0900

    New Study !!!

---
+++ src/com/jhj/object/class01/RectangleShape.java
@@ -0,0 +1,89 @@
+/**
+ * File Name: RectangleShape.java
+ * Copyright: Copyright (c) RedRooster Co., Ltd 2020. All rights reserved.
+ * Description: 
+ * Creator: Hyungjun, Jeon
+ * Created Date: 2020. 7. 22
+ */
+package com.jhj.object.class01;
+
+/**
+ * <p/>
+ * @since 2020. 7. 22
+ * @author Hyungjun, Jeon
+ * <PRE>
+ * ------------------------------------------------
+ * 수정이력
+ * 2020. 7. 22 Hyungjun, Jeon
+ * </PRE>
+ */
+public class RectangleShape {
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	
+	/**
+	 * 
+	 */
+	public RectangleShape() {}
+
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	/**
+	 * @return the width
+	 */
+	public int getWidth() {
+		return width;
+	}
+
+	/**
+	 * @param width the width to set
+	 */
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	/**
+	 * @return the height
+	 */
+	public int getHeight() {
+		return height;
+	}
+
+	/**
+	 * @param height the height to set
+	 */
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	
+
+}
